774e474d9a1968dcc30e2f09c7be7f44174009cd
Pavlenov Semen <me@pavlenov.ru>
2017-01-29 20:07:52 +0300

    Add engines

---
+++ rest-server/src/main/scala/com/actionml/entity/Engine.scala
@@ -0,0 +1,11 @@
+package com.actionml.entity
+
+/**
+  *
+  *
+  * @author The ActionML Team (<a href="http://actionml.com">http://actionml.com</a>)
+  * 29.01.17 17:58
+  */
+
+case class EngineId(engineId: String)
+case class Engine(id: String)
---
+++ rest-server/src/main/scala/com/actionml/service/EngineService.scala
@@ -0,0 +1,40 @@
+package com.actionml.service
+
+import com.actionml.ActorInjectable
+import com.actionml.entity.Engine
+import scaldi.Injector
+
+/**
+  *
+  *
+  * @author The ActionML Team (<a href="http://actionml.com">http://actionml.com</a>)
+  * 28.01.17 14:49
+  */
+
+trait EngineService extends ActorInjectable
+
+class EmptyEngineService(implicit inj: Injector) extends EngineService{
+  override def receive: Receive = {
+    case GetEngine(engineId) ⇒
+      log.info("Get engine, {}", engineId)
+      sender() ! None
+
+    case CreateEngine(engine) ⇒
+      log.info("Create new engine, {}", engine)
+      sender() ! None
+
+    case UpdateEngine(engineId, engine) ⇒
+      log.info("Update exist engine, {}, {}", engineId, engine)
+      sender() ! None
+
+    case DeleteEngine(engineId) ⇒
+      log.info("Delete exist engine, {}", engineId)
+      sender() ! None
+  }
+}
+
+sealed trait EngineAction
+case class GetEngine(engineId: String) extends EngineAction
+case class CreateEngine(engine: Engine) extends EngineAction
+case class UpdateEngine(engineId: String, engine: Engine) extends EngineAction
+case class DeleteEngine(engineId: String) extends EngineAction
