73fe9ad16510022ed8de14150e142b5ebbccfece
Semen Pavlenov <semen.pavlenov@gmail.com>
2017-03-04 13:08:13 +0300

    uncomment ssl

--- rest-server/src/main/scala/com/actionml/router/http/RestServer.scala
+++ rest-server/src/main/scala/com/actionml/router/http/RestServer.scala
@@ -1,13 +1,17 @@
 package com.actionml.router.http
 
+import java.security.{KeyStore, SecureRandom}
+import javax.net.ssl.{KeyManagerFactory, SSLContext, TrustManagerFactory}
+
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
+import akka.http.scaladsl.{ConnectionContext, Http}
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import com.actionml.router.config.AppConfig
 import com.actionml.router.http.directives.{CorsSupport, LoggingSupport}
 import com.actionml.router.http.routes.{DatasetsRouter, EnginesRouter, EventsRouter, QueriesRouter}
 import akka.http.scaladsl.server.Directives._
+import com.typesafe.sslconfig.akka.AkkaSSLConfig
 import scaldi.Injector
 import scaldi.akka.AkkaInjectable
 
@@ -34,33 +38,33 @@ class RestServer(implicit inj: Injector) extends AkkaInjectable with CorsSupport
   private val route: Route = events.route ~ datasets.route ~ engines.route ~ queries.route
 
   def run(host: String = config.host, port: Int = config.port): Future[Http.ServerBinding] = {
-//    if (config.ssl) {
-//      Http().setDefaultServerHttpContext(https)
-//    }
+    if (config.ssl) {
+      Http().setDefaultServerHttpContext(https)
+    }
     Http().bindAndHandle(logResponseTime(route), host, port)
   }
 
-//  private def https = {
-//
-//    val sslConfig = AkkaSSLConfig()
-//
-//    val password: Array[Char] = sslConfig.config.keyManagerConfig.keyStoreConfigs.head.password.get.toCharArray
-//
-//    val ks: KeyStore = KeyStore.getInstance("JKS")
-//    val keystore: InputStream = getClass.getClassLoader.getResourceAsStream("keys/localhost.jks")
-//
-//    require(keystore != null, "Keystore required!")
-//    ks.load(keystore, password)
-//
-//    val keyManagerFactory: KeyManagerFactory = KeyManagerFactory.getInstance("SunX509")
-//    keyManagerFactory.init(ks, password)
-//
-//    val tmf: TrustManagerFactory = TrustManagerFactory.getInstance("SunX509")
-//    tmf.init(ks)
-//
-//    val sslContext: SSLContext = SSLContext.getInstance("TLS")
-//    sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
-//    ConnectionContext.https(sslContext)
-//  }
+  private def https = {
+
+    val sslConfig = AkkaSSLConfig()
+
+    val password: Array[Char] = sslConfig.config.keyManagerConfig.keyStoreConfigs.head.password.get.toCharArray
+
+    val ks = KeyStore.getInstance("JKS")
+    val keystore = getClass.getClassLoader.getResourceAsStream("keys/localhost.jks")
+
+    require(keystore != null, "Keystore required!")
+    ks.load(keystore, password)
+
+    val keyManagerFactory = KeyManagerFactory.getInstance("SunX509")
+    keyManagerFactory.init(ks, password)
+
+    val tmf = TrustManagerFactory.getInstance("SunX509")
+    tmf.init(ks)
+
+    val sslContext = SSLContext.getInstance("TLS")
+    sslContext.init(keyManagerFactory.getKeyManagers, tmf.getTrustManagers, new SecureRandom)
+    ConnectionContext.https(sslContext)
+  }
 
 }
