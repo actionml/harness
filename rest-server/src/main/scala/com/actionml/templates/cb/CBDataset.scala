5d44d831865d08220e6afacb134b4d7736ee9975
pferrel <pat@occamsmachete.com>
2017-03-06 15:53:57 -0800

    wrote a command line spec

--- rest-server/src/main/scala/com/actionml/templates/cb/CBDataset.scala
+++ rest-server/src/main/scala/com/actionml/templates/cb/CBDataset.scala
@@ -3,7 +3,21 @@ package com.actionml.templates.cb
 import com.actionml.core.storage.Store
 import com.actionml.core.template.Dataset
 
+/** Implements the persistence for the Contextual Bandit
+  * There are 2 types of events for the CB 1) usage events and 2) property change events. The usage events
+  * are stored as a potentially very large collection, the propety change events translate into changes to
+  * mutable DB objects.
+  *
+  * See the discussion of Kappa Learning here: https://github.com/actionml/pio-kappa/blob/master/kappa-learning.md
+  *
+  * @param resourceId REST resource-id for input
+  * @param store where they are stored
+  */
 class CBDataset(resourceId: String, store: Store) extends Dataset[CBEvent](resourceId, store) {
+  // the resourceId is used as a db-id in Mongo, under which will be collections/tables for
+  // useage events (which have a datetime index and a TTL) and mutable tables that reflect state of
+  // input like groups, and even ML type models created by Vowpal Wabbit's Contextual Bandit algo. The later
+  // do not have TTL and reflect the state of the last watermarked model update.
 
   var events = Seq[CBEvent]() // todo: this will go in a Store eventually
 
@@ -20,6 +34,5 @@ class CBDataset(resourceId: String, store: Store) extends Dataset[CBEvent](resou
     events = events ++: data
     Seq[Boolean](true)
   }
-
 
 }
