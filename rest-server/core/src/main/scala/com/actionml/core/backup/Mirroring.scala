510a5ed8eea4b9243090353f22d0dceed87af9a8
snovgorodtsev <snovgorodtsev@evolutiongaming.com>
2017-05-25 23:39:10 +0300

    json mirroring

--- rest-server/core/src/main/scala/com/actionml/core/backup/Mirroring.scala
+++ rest-server/core/src/main/scala/com/actionml/core/backup/Mirroring.scala
@@ -6,10 +6,23 @@ import java.time.{LocalDateTime, ZoneId}
 
 import scala.util.Try
 
+/**
+  *
+  * That's a very basic implementation of JSON request backing up.
+  * The trait could be mixed in any class which needs the mirroring.
+  *
+  */
 trait Mirroring {
-  private val dirNamePattern = DateTimeFormatter.ofPattern("d-MMM-yyyy")
+  private val dirNamePattern = DateTimeFormatter.ofPattern("dd-MMM-yyyy")
   private val fileNamePattern = DateTimeFormatter.ofPattern("HH-mm-ss.SSS")
 
+  /**
+    *
+    * The method mirrors the input JSON to a file called HH-mm-ss.SSS.json
+    * It's placed in a folder with a date-specific name
+    *
+    * @param json input JSON
+    */
   def mirrorJson(json: String): Unit = {
     val now = LocalDateTime.now(ZoneId.of("UTC"))
     val directory = new File(dirNamePattern.format(now))
