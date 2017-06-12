37124c3125ae17fc4e30b9e82ec433ca0d30a07b
Semen Pavlenov <semen.pavlenov@gmail.com>
2017-06-12 14:48:43 +0300

    Add change for integration tests

--- rest-server/entrypoint.sh
+++ rest-server/entrypoint.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 sleep infinity
+
+#export REST_SERVER_HOST=${REST_SERVER_HOST:-localhost}
+#export REST_SERVER_PORT=${REST_SERVER_PORT:-9090}
+#
+#export MONGO_HOST=${MONGO_HOST:-localhost}
+#export MONGO_PORT=${MONGO_PORT:-27017}
+#
+#export HARNESS_LOG_CONFIG="/app/conf/logback.xml"
+#export HARNESS_LOG_PATH="$/app/logs"
+#
+#exec /app/bin/main \
+#-Dlogback.configurationFile=${HARNESS_LOG_CONFIG} \
+#-DHARNESS_LOG_PATH=${HARNESS_LOG_PATH} \
+#-Drest-server.host=${REST_SERVER_HOST} \
+#-Drest-server.port=${REST_SERVER_PORT} \
+#-Dmongo.host=${MONGO_HOST} \
+#-Dmongo.port=${MONGO_PORT}
---
+++ rest-server/server/src/main/java/com/actionml/Utils.java
@@ -0,0 +1,74 @@
+package com.actionml;
+
+import vw.jni.NativeUtils;
+
+import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author The ActionML Team (<a href="http://actionml.com">http://actionml.com</a>)
+ * Created by Pavlenov Semen on 11.06.17.
+ */
+public class Utils {
+
+    private static String getDistroName() throws IOException {
+        Pattern distroRegex = Pattern.compile("[^(]+\\([^(]+\\([^(]+\\(([A-Za-z\\s]+).*");
+        BufferedReader reader = new BufferedReader(new FileReader("/proc/version"));
+        String distro;
+        try {
+            Matcher line = distroRegex.matcher(reader.readLine());
+            distro = line.matches() ? line.group(1) : null;
+        }
+        finally {
+            reader.close();
+        }
+        return distro;
+    }
+
+    private static String getLinuxDistro() throws IOException {
+        BufferedReader reader = null;
+        String release = null;
+        String distro = getDistroName();
+        try {
+            Process process = Runtime.getRuntime().exec("lsb_release -r");
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            Pattern releasePattern = Pattern.compile("Release:\\s*(\\d+).*");
+            Matcher matcher;
+            while ((line = reader.readLine()) != null) {
+                matcher = releasePattern.matcher(line);
+                if (matcher.matches()) {
+                    release = matcher.group(1);
+                }
+            }
+        }
+        finally {
+            reader.close();
+        }
+        if (distro == null || release == null) {
+            throw new UnsupportedEncodingException("Linux distro does not support lsb_release, cannot determine version, distro: " + distro + ", release: " + release);
+        }
+
+        return distro.trim().replaceAll(" ", "_") + "." + release;
+    }
+
+    private static String getOsFamily() throws IOException {
+        final String osName = System.getProperty("os.name");
+        if (osName.toLowerCase().contains("mac")) {
+            return "Darwin";
+        }
+        else if (osName.toLowerCase().contains("linux")) {
+            return getLinuxDistro();
+        }
+        throw new IllegalStateException("Unsupported operating system " + osName);
+    }
+
+    public static String loadOSDependentLibrary() throws IOException {
+        String osFamily = getOsFamily();
+        return osFamily + "." + System.getProperty("os.arch");
+
+    }
+
+
+}
