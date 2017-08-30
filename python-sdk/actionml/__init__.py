4c215b987a013092acda471669930e058741490a
Andrey Shikov <andrey.shikov@gmail.com>
2017-08-30 19:58:54 +0300

    Added permissions method and example

--- python-sdk/actionml/__init__.py
+++ python-sdk/actionml/__init__.py
@@ -461,7 +461,7 @@ class CommandClient(BaseClient):
 
 class UsersClient(BaseClient):
 
-    def __init__(self, url="http://localhost:9090", threads=1, qsize=0, timeout=5, client_id=None, client_secret=None):
+    def __init__(self, client_id, client_secret, url="http://localhost:9090", threads=1, qsize=0, timeout=5):
         self.path = "/auth/users"
         super().__init__(url, threads, qsize, timeout, client_id, client_secret)
 
@@ -474,14 +474,23 @@ class UsersClient(BaseClient):
 
 class PermissionsClient(BaseClient):
 
-    def __init__(self, user_perm_id, url="http://localhost:9090", threads=1, qsize=0, timeout=5):
-        self.path = "/auth/users/{}/permissions".format(user_perm_id)
-        super().__init__(url, threads, qsize, timeout)
+    def __init__(self, client_id, client_secret, url="http://localhost:9090", threads=1, qsize=0, timeout=5):
+        self.path = "/auth/users"
+        super().__init__(url, threads, qsize, timeout, client_id, client_secret)
 
-    def grant_permission(self, user_id=None, user_secret=None, user_perm_id=None, role_set_id=None, resource_id=None):
-        request = AsyncRequest("POST", self.path, user_id=user_id, user_secret=user_secret,
-                               roleSetId=role_set_id, resourceId=resource_id, userPermId=user_perm_id)
+    def grant_permission(self, user_id, user_secret, permitted_user_id, role_set_id, resource_id):
+        path = self.path + "/{}/permissions".format(permitted_user_id)
+        request = AsyncRequest("POST", path, user_id=user_id, user_secret=user_secret,
+                               roleSetId=role_set_id, resourceId=resource_id)
         request.set_response_handler(self._ok_response_handler)
         self._connection.make_request(request)
-        return request
+        return request.get_response()
+
+    def revoke_permission(self, user_id, user_secret, permitted_user_id, role_set_id):
+        path = self.path + "/{}/permissions".format(permitted_user_id)
+        request = AsyncRequest("DELETE", path, user_id=user_id, user_secret=user_secret,
+                               roleSetId=role_set_id)
+        request.set_response_handler(self._ok_response_handler)
+        self._connection.make_request(request)
+        return request.get_response()
 
