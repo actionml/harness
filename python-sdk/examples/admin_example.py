564cdf94ceb0c3d20900b1fa9286819694cc9ed5
Andrey Shikov <andrey.shikov@gmail.com>
2017-09-07 21:15:14 +0300

    Fixed ssl support in python sdk

--- python-sdk/examples/admin_example.py
+++ python-sdk/examples/admin_example.py
@@ -3,8 +3,8 @@ import os
 
 if __name__ == '__main__':
     url = "http://localhost:9090"
-    user_id = os.getenv('HARNESS_USER_ID', None)
-    user_secret = os.getenv('HARNESS_USER_SECRET', None)
+    user_id = os.getenv('HARNESS_CLIENT_USER_ID', None)
+    user_secret = os.getenv('HARNESS_CLIENT_USER_SECRET', None)
 
     users_client = actionml.UsersClient(url=url, threads=5, qsize=500, user_id=user_id, user_secret=user_secret)
     response = users_client.create_user(role_set_id="admin", resource_id="*")
