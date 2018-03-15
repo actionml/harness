d22be5495342f91a4510d17bc7726e46ad53bbf4
pferrel <pat@occamsmachete.com>
2018-03-15 05:07:50 -0700

    cleaned up the integration tests, added several options including a db wipe

--- java-sdk/example/integration-test-3.sh
+++ java-sdk/example/integration-test-3.sh
@@ -5,7 +5,7 @@ echo "Usage: ./integration-test-3.sh"
 #"Usage: ./send_queries host resource-id queries-file.json"
 echo
 host=localhost
-resource_id=test_resource
+resource_id=test_cb
 test_3_phase_1_data=data/test_3_phase_1.json
 test_3_phase_2_data=data/test_3_phase_2.json
 test_3_phase_3_data=data/test_3_phase_3.json
