3f85cfdc7685aea5a9bdf4f0499a1a1c1e118a54
pferrel <pat@occamsmachete.com>
2019-06-02 16:20:26 -0700

    cleanup of unused files

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://github.com/actionml/harness/archive/develop.zip
-unzip develop.zip
-./harness-develop/rest-server/make-distribution.sh
-tar zxvf ./harness-develop/rest-server/Harness-0.1.0-SNAPSHOT.tar.gz
-echo './Harness-0.1.0-SNAPSHOT/bin/harness -help'
-./Harness-0.1.0-SNAPSHOT/bin/harness -help
