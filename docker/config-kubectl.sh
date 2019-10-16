7d98bad4aeec133179a4baddd5931d92d82d50d3
Dioni <dioni@ezops.com.br>
2019-10-16 12:22:15 -0300

    Full test

--- docker/config-kubectl.sh
+++ docker/config-kubectl.sh
@@ -5,7 +5,14 @@ chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
 
-kubectl config set-credentials admin --username=${K8S_ADMIN_USER} --password=${K8S_ADMIN_PASS}
-kubectl config set-cluster k8s.sitespect --insecure-skip-tls-verify=true --server=${K8S_SERVER}
-kubectl config set-context k8s.sitespect --user=admin --namespace=default --cluster=k8s.sitespect
-kubectl config use-context k8s.sitespect
+# kubectl config set-credentials admin --username=${K8S_ADMIN_USER} --password=${K8S_ADMIN_PASS}
+# kubectl config set-cluster k8s.sitespect --insecure-skip-tls-verify=true --server=${K8S_SERVER}
+# kubectl config set-context k8s.sitespect --user=admin --namespace=default --cluster=k8s.sitespect
+# kubectl config use-context k8s.sitespect
+
+kubectl config set-credentials admin --username=${K8S_ADMIN_USER_DEVOPS} --password=${K8S_ADMIN_PASS_DEVOPS}
+kubectl config set-cluster devops.dev --insecure-skip-tls-verify=true --server=${K8S_SERVER_DEVOPS}
+kubectl config set-context devops.dev --user=admin --namespace=default --cluster=devops.dev
+kubectl config use-context devops.dev
+
+
