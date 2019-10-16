5dd990f3244dd5237fec1bd0e7445c3c3f7c5679
Dioni <dioni@ezops.com.br>
2019-10-16 11:26:39 -0300

    Test

--- docker/config-kubectl.sh
+++ docker/config-kubectl.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-   curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-   chmod +x ./kubectl
-   mv ./kubectl /usr/local/bin/kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
 
-   kubectl version
+
+kubectl config set-credentials admin --username=${K8S_ADMIN_USER} --password=${K8S_ADMIN_PASS}
+kubectl config set-cluster k8s.sitespect --insecure-skip-tls-verify=true --server=${K8S_SERVER}
+kubectl config set-context k8s.sitespect --user=admin --namespace=default --cluster=k8s.sitespect
+kubectl config use-context k8s.sitespect
