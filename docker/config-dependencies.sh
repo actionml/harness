dae1289643f0c48c0cee8269049abf08926550d7
Dioni <dioni@ezops.com.br>
2019-10-16 14:41:35 -0300

    Cleanup

--- docker/config-dependencies.sh
+++ docker/config-dependencies.sh
@@ -10,16 +10,12 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
 chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
 
-
 # == Configure K8s Cluster
-# kubectl config set-credentials admin --username=${K8S_ADMIN_USER} --password=${K8S_ADMIN_PASS}
-# kubectl config set-cluster k8s.sitespect --insecure-skip-tls-verify=true --server=${K8S_SERVER}
-# kubectl config set-context k8s.sitespect --user=admin --namespace=default --cluster=k8s.sitespect
-# kubectl config use-context k8s.sitespect
+kubectl config set-credentials admin --username=${K8S_ADMIN_USER} --password=${K8S_ADMIN_PASS}
+kubectl config set-cluster k8s.sitespect --insecure-skip-tls-verify=true --server=${K8S_SERVER}
+kubectl config set-context k8s.sitespect --user=admin --namespace=default --cluster=k8s.sitespect
+kubectl config use-context k8s.sitespect
 
-kubectl config set-credentials admin --username=${K8S_ADMIN_USER_DEVOPS} --password=${K8S_ADMIN_PASS_DEVOPS}
-kubectl config set-cluster devops.dev --insecure-skip-tls-verify=true --server=${K8S_SERVER_DEVOPS}
-kubectl config set-context devops.dev --user=admin --namespace=default --cluster=devops.dev
-kubectl config use-context devops.dev
+
 
 
