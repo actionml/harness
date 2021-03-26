05bfd1c1b2a520b10ee48fc6c639cd1d3043557c
Dioni <dioni@ezops.com.br>
2021-03-26 10:46:35 -0300

    added git

--- docker/config-dependencies.sh
+++ docker/config-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # == Install docker composer
-apk add --no-cache --update py-pip python3-dev libffi-dev openssl-dev gcc libc-dev make curl curl-dev
+apk add --no-cache --update py-pip python3-dev libffi-dev openssl-dev gcc libc-dev make curl curl-dev git
 pip install docker-compose==1.12.0
 docker-compose version 
